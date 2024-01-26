f55488e59ec1197132153ea49e500d4a6570e29d
Ronit Kene <97221143+Ig-Ronit@users.noreply.github.com>
2024-01-26 12:37:53 +0530

    MyConnection.java

--- MyConnection.java
+++ MyConnection.java
@@ -6,7 +6,7 @@ public class MyConnection {
 	{
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ronit","root","root");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/database_name","password","password");
 			
 		} catch (Exception e) {
 			System.out.println(e);
